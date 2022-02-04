5cb09f6dfb7a1772fbca32a3460c7ae5c3ffca31
Rizki36 <cahrizki.rf@gmail.com>
2022-02-04 19:15:37 +0700

    feat(server): add route logout

--- packages/backend/src/resources/auth/auth.route.ts
+++ packages/backend/src/resources/auth/auth.route.ts
@@ -3,12 +3,13 @@ import express from 'express';
 import verifyToken from '../../middleware/verifToken';
 import { authorization } from '../../middleware/authorization';
 import validate from '../../middleware/validation';
-import { signupSchema } from './auth.validation';
+import { signupSchema, signinSchema } from './auth.validation';
 
 const route = express.Router();
 
 route.post('/signup', validate(signupSchema), AuthController.signup)
-    .post('/signin', AuthController.signin)
-    .get('/account', verifyToken, authorization(['USER']), AuthController.account);
+    .post('/signin', validate(signinSchema), AuthController.signin)
+    .get('/account', verifyToken, authorization(['USER']), AuthController.account)
+    .post('/logout', verifyToken, AuthController.logout)
 
 export default route;
